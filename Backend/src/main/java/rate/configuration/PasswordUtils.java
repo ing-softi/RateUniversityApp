66557603141fbabda7476868fb6598aa552c2cb7
KleardoDura <111565373+KleardoDura@users.noreply.github.com>
2024-01-15 21:46:51 +0100

    Create PasswordUtils.java

---
+++ Backend/src/main/java/rate/configuration/PasswordUtils.java
@@ -0,0 +1,13 @@
+package rate.configuration;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtils {
+    public static String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+
+    public static boolean verifyPassword(String plainTextPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainTextPassword, hashedPassword);
+    }
+}
